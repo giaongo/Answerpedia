f569d5a8d3c1692cc140fbda37637821ffeed282
Giao Ngo <giao.ngo@metropolia.fi>
2022-12-01 10:11:45 +0200

    update the base foundation for app, add questionModel, questionRoute and questionControllers. Add connection pool in passport.js. Contribute to #7

--- server/app.js
+++ server/app.js
@@ -1,2 +1,14 @@
 'use strict';
-console.log('Hello World');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3000;
+const questionRouter = require("./routes/questionRoute");
+
+app.use(express.static("uploads"));
+app.use("/thumbnails",express.static("thumbnails"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.listen(port,() => console.log(`App listening on port ${port}!`));
