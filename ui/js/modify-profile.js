bc253784d92c0e43a7587bf863914ade8ef15eda
Tai Nguyen <phatn@metropolia.fi>
2022-12-04 22:26:21 +0200

    added logout.html and logout.js. Contritbute to #5

---
+++ ui/js/modify-profile.js
@@ -0,0 +1,5 @@
+const uploadPictureBtn = document.querySelector('#upload-picture-button');
+const uploadPictureInput = document.querySelector('#upload-picture-input');
+uploadPictureBtn.addEventListener('click', () => {
+    uploadPictureInput.click();
+})
